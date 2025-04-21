105274371fe2485c2c7f57e18bf6c54405399807
dhyan14 <dhyanjain341988@gmail.com>
2025-04-21 16:25:43 +0530

    aaa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    // Ignore TypeScript errors in production builds
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Ignore ESLint errors in production builds
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig 
